6b2b99ec3980753ab69d12489fe396a6d5f3ed53
nitr-am <martin7gomez@hotmail.com>
2019-10-22 20:20:00 +0000

    Another big commit

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 import os
-from flask import Flask, render_template
+import json
+from flask import Flask, render_template, request, flash
 # First we are importing a Flask class.
 # This is an instance of the Flask class.
 app = Flask(__name__)
+app.secret_key = 'some_secret'
 # The first argument of the Flask class is the name of the package, as we are using a single module, I can use __name__
 # Which is a built in Python module.
 
@@ -13,16 +15,35 @@ def index():
 
 @app.route('/about')
 def about():
-    return render_template("about.html")
+    data = []
+    with open('data/company.json', 'r') as json_data:
+        data = json.load(json_data)
+    return render_template("about.html", page_title='About', company=data)
+
+@app.route('/about/<member_name>')
+def about_member(member_name):
+    member = {}
+
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+
+    return render_template("member.html", member=member)
 
 
-@app.route('/contact')
+@app.route('/contact', methods = ['GET', 'POST'])
 def contact():
-    return render_template("contact.html")
+    if request.method == 'POST':
+        flash("Thanks {}, we have received your message.".format(
+            request.form["name"]
+        ))
+    return render_template("contact.html", page_title='Contact')
 
 @app.route('/careers')
 def careers():
-    return render_template("careers.html")
+    return render_template("careers.html", page_title='Careers')
 
 # if __name__ == '__main__':
 #     app.run(host=os.environ.get('IP'),
